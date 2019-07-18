4273d297a6a72092cb855f7e4bd41ae26cb57d89
Thalison Wilker <thalison.wilker@gmail.com>
2019-07-18 17:45:32 -0300

    react + core do bootstrap

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { Button, Container, Row, Col } from 'reactstrap'
 
-export default props => <h1>
-    React ok!
-</h1>
+export default props => <Container className="py-5">
+    <Row>
+        <Col col={12}>
+            <h1 className="text-center">
+                React bootstrap
+            </h1>
+            <Button color="primary">
+                Yes!
+            </Button>
+        </Col>
+    </Row>
+</Container>
